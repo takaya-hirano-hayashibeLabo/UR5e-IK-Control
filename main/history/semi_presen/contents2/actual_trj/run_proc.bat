3d4e99aa9bb612e88d8eb411e4c350a833e199b9
takaya-hirano-hayashibeLabo <takaya.hirano.p5@dc.tohoku.ac.jp>
2024-10-31 15:40:42 +0900

    add: semi-presen/ content2/actual trj

---
+++ main/history/semi_presen/contents2/actual_trj/run_proc.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+cd /d %~dp0
+call C:\Users\3meko\miniconda3\Scripts\activate.bat
+call conda activate mink-env
+
+set TIMESCALE_LIST=0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 2 3 4 5 6 7 8 9 10
+set MODELPATH=C:\Users\3meko\Dev\HayashibeLab\workspace\DynamicSNN\train-trajectory\output\20241024\circle_beta0.8_identity_noise0.05
+set TRJPATH=C:\Users\3meko\Dev\HayashibeLab\workspace\ur5e_ik_control\main\collect_dataset\20241024\circle\output\datasets.csv
+
+
+for %%t in (%TIMESCALE_LIST%) do (
+    python get_actual_trj.py --modelpath %MODELPATH% --trjpath %TRJPATH% --timescale %%t --saveto 20241031_output/dynasnn --nloop 10
+)
+
+endlocal
--- main/history/semi_presen/contents2/ideal_trj/run_proc.bat
+++ main/history/semi_presen/contents2/ideal_trj/run_proc.bat
@@ -12,13 +12,3 @@ for %%t in (%TIMESCALE_LIST%) do (
 )
 
 endlocal
-
-
-@REM TIMESCALE_LIST=(
-@REM     0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-@REM     1 2 3 4 5 6 7 8 9 10
-@REM )
-
-@REM for timescale in ${TIMESCALE_LIST[@]};do
-@REM     python get_ideal_trj.py --configpath conf.yml --timescale $timescale --saveto 20241031_output
-@REM done
